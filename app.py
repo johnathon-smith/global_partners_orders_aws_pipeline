ec08fc6851ea9e45df72d3f5c67cefe30c516bf0
Johnathon Smith <85951765+johnathon-smith@users.noreply.github.com>
2026-05-20 13:18:56 -0400

    Update app.py

--- app.py
+++ app.py
@@ -294,13 +294,18 @@ with tabs[0]:
                 .astype(float)
             )
 
+            st.dataframe(top_locations[["restaurant_id", "total_revenue"]])
+            st.bar_chart(
+                top_locations.set_index("restaurant_id")["total_revenue"]
+            )
+
             chart = (
                 alt.Chart(top_locations)
                 .mark_bar()
                 .encode(
                     x=alt.X("total_revenue:Q", title="Total Revenue", axis=alt.Axis(format=",.2f")),
-                    y=alt.Y("restaurant_id:N", title="Restaurant", sort="-x"),
-                    tooltip=["restaurant_id", alt.Tooltip("total_revenue:Q", title="Total Revenue", format="$,2f")],
+                    y=alt.Y("restaurant_id:N", title="Restaurant", sort=top_locations["restaurant_id"].tolist()),
+                    tooltip=["restaurant_id", alt.Tooltip("total_revenue:Q", title="Total Revenue", format=",2f")],
                 )
             )
             st.altair_chart(chart, use_container_width=True)
